251313fa74b82ca8d684a5becacd2c5e0418bb49
danielabarca <danielabarcamoya@gmail.com>
2019-08-20 17:48:00 -0400

    Entrega desafío 2

---
+++ Aux3/auxiliar-3-danielabarca-master/in3501/primavera19/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import * 
+
+urlpatterns = [
+	path('', index, name ='index'),
+	path('inputs/', inputs, name='inputs'),
+	path('formulario/', formulario, name='formulario')
+
+
+]
---
+++ Aux3/auxiliar-3-danielabarca-master/in3501/primavera19/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'home.html')
+
+def inputs(request):
+	return render(request, 'inputs.html')
+
+def formulario(request):
+	return render(request, 'formulario.html')
